af997bdc31a9c5994ee0fa114458383329b63e5e
rozhkovetskiy <rozhkovetskiy@gmail.com>
2017-10-19 14:57:29 +0300

    Step 2. Add datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,25 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { AddTaskComponent } from './add-task/add-task.component';
 
 import { ListService } from './shared/services/list.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    AddTaskComponent
   ],
   imports: [
     FormsModule,
     BrowserModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot()
   ],
   providers: [ListService],
   bootstrap: [AppComponent]
